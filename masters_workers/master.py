715eb8ce714dc626f971c698e4c8cb6ff17431e4
Sina Rasouli <sina.rasouli@cafebazaar.ir>
2021-11-23 00:16:36 +0330

    add master to masters_workers

--- masters_workers/master.py
+++ masters_workers/master.py
@@ -1,11 +1,49 @@
 # Modules
+import multiprocessing as mp
 
 # Parameters
 
 # Methods
 
 # Classes
+class Master(mp.Process):
+    # Constructor
+    def __init__(self, name, queue_tasks, queue_logger):
+        super().__init__()
+        self.name = name
+        self.queue_tasks = queue_tasks
+        self.queue_logger = queue_logger
+        return
+    # Generate Tasks
+    def run(self):
+        # Generate Tasks
+        for i in range(10):
+            # Add Task to Queue for Workers
+            self.queue_tasks.put(i)
+            self.log( f"task {i} created", 0 )
+        # Return
+        return
+    # STR
+    def __str__(self):
+        return f"<Master@{self.name}>"
+    # Log
+    def log(self, msg, log_level):
+        self.queue_logger.put((
+            str(self),
+            msg,
+            log_level
+        ))
+        return
 
 # Main
 if __name__ == "__main__":
-    pass
+    # Parameters
+    queue_tasks, queue_logger = mp.Queue(100), mp.Queue()
+    # Create Master
+    master = Master("main", queue_tasks, queue_logger)
+    # Start Master
+    master.start()
+    master.join()
+    # Report
+    print( f"Queue Size of Tasks : {queue_tasks.qsize()}" )
+    print( f"Queue Size of Logs  : {queue_logger.qsize()}" )
